d7cf9738bdac14e281c4ffd72ad4eca60f6a86eb
toptuk49 <69755076+toptuk49@users.noreply.github.com>
2022-10-21 21:34:36 +0300

    Added switching

--- js/script.js
+++ js/script.js
@@ -15,3 +15,36 @@ function testWebP(callback) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+
+
+let currencies = document.querySelectorAll('.currency-item');
+let card = document.querySelectorAll('.card-container');
+
+for (let i = 0; i < currencies.length; i++) {
+    currencies[i].addEventListener('click', function() {
+        for (let a = 0; a < currencies.length; a++) {
+            if (currencies[a].classList.contains('selected')) {
+                currencies[a].classList.remove('selected');
+            }
+        }
+
+        currencies[i].classList.add('selected');
+
+        for (let a = 0; a < currencies[i].childNodes.length; a++) {
+            let currency = currencies[i].childNodes[a];
+            if (currency.tagName === 'SPAN') {
+                let currency_name = currency.innerHTML;
+
+                if (currency_name === 'USDT') {
+                    for (let b = 0; b < card.length; b++) {
+                        card[b].style.display = 'flex';
+                    }
+                } else {
+                    for (let b = 0; b < card.length; b++) {
+                        card[b].style.display = 'none';
+                    }
+                }
+            }
+        }
+    });
+}
